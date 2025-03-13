c44b2da19a06459ed0274e725fcbddaa1589572f
rtisba <r.tisba@ecole-ipssi.net>
2025-03-13 22:35:37 +0100

    fix: css profil

--- front-end/src/pages/Profile.tsx
+++ front-end/src/pages/Profile.tsx
@@ -54,108 +54,84 @@ export function Profile() {
   const tabs = [
     { name: "Tweets", path: "tweets" },
   ];
-
   return (
-    <>
-      <header className="sticky top-0 z-10 border-b border-gray-200 bg-white/80 p-4 backdrop-blur">
-        <div className="flex items-center space-x-4">
-          <h1 className="text-xl font-bold">{currentUser.pseudo}</h1>
-        </div>
-      </header>
-
-      <div className="relative">
+    <div className="profile-container mt-5">
+      {/* Bannière de profil */}
+      <div
+        className="profile-banner"
+        style={{ backgroundImage: `url(${currentUser.banniere || "https://picsum.photos/800/600?random=1&blur=2"})` }}
+      >
         <img
-          src={currentUser.banniere ? currentUser.banniere : "https://picsum.photos/800/600?random=1&blur=2"}
-          alt="Profile banner"
-          className="h-48 w-full object-cover"
+          src={currentUser.photo || "https://picsum.photos/199?random=2"}
+          alt={currentUser.pseudo}
+          className="profile-avatar"
         />
-        <div className="absolute -bottom-16 left-4">
-          <img
-            src={currentUser.photo ? currentUser.photo : "https://picsum.photos/199?random=2"}
-            alt={currentUser.pseudo}
-            className="h-32 w-32 rounded-full border-4 border-white"
+      </div>
+
+      {/* Informations utilisateur */}
+      <div className="profile-header">
+        <div>
+          <h2>{currentUser.pseudo}</h2>
+          <p className="profile-username">@{currentUser.pseudo}</p>
+        </div>
+        <Button variant="outline">Edit profile</Button>
+      </div>
+
+      <div className="profile-info">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+
+        <div className="profile-links">
+          <div className='flex'>
+            <MapPin size={16} /> Saint-quentin-en-yvelines, France
+          </div>
+          <div className='flex'>
+            <LinkIcon size={16} />
+            <a href="https://ecole-ipssi.com/" target="_blank"> https://ecole-ipssi.com/ </a>
+          </div>
+          <div className='flex'>
+            <Calendar size={16} /> Joined {currentUser.joined}
+          </div>
+        </div>
+
+        <div className="profile-stats">
+          <span>{currentUser.following} Following</span>
+          <span>{currentUser.followers} Followers</span>
+        </div>
+      </div>
+
+      {/* Navigation entre les sections */}
+      <div className="profile-tabs">
+        {tabs.map((tab) => (
+          <Link
+            key={tab.name}
+            to={`/profile/${id}/${tab.path}`}
+            className={`profile-tab ${location.pathname === `/profile/${id}/${tab.path}` ? "active" : ""}`}
+          >
+            {tab.name}
+          </Link>
+        ))}
+      </div>
+
+      {/* Contenu dynamique des onglets */}
+      <div className="profile-content">
+        <Outlet />
+      </div>
+
+      {/* Liste des tweets */}
+      {tweets.length > 0 ? (
+        tweets.map((tweet) => (
+          <TweetCard
+            key={tweet._id}
+            tweet={tweet}
+            onLike={() => {}}
+            onRetweet={() => {}}
+            onReply={() => {}}
+            onBookmark={() => {}}
           />
-        </div>
-      </div>
-
-      <div className="mb-4 mt-20 px-4">
-        <div className="flex justify-end">
-          <Button variant="outline">Edit profile</Button>
-        </div>
-
-        <div className="mt-4">
-          <h2 className="text-xl font-bold">{currentUser.pseudo}</h2>
-          <p className="text-gray-500">@{currentUser.pseudo}</p>
-          <p className="mt-4">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-
-          <div className="mt-4 flex flex-wrap gap-4 text-gray-500">
-            <div className="flex items-center">
-              <MapPin className="mr-1 h-4 w-4" />
-              Saint-quentin-en-yvelines, France
-            </div>
-            <div className="flex items-center">
-              <LinkIcon className="mr-1 h-4 w-4" />
-              <a href="https://ecole-ipssi.com/" target="_blank" className="text-blue-500">https://ecole-ipssi.com/</a>
-            </div>
-            <div className="flex items-center">
-              <Calendar className="mr-1 h-4 w-4" />
-              Joined {currentUser.joined}
-            </div>
-          </div>
-
-          <div className="mt-4 flex space-x-4">
-            <span>
-              <span className="font-bold">{currentUser.following}</span>{' '}
-              <span className="text-gray-500">Following</span>
-            </span>
-            <span>
-              <span className="font-bold">{currentUser.followers}</span>{' '}
-              <span className="text-gray-500">Followers</span>
-            </span>
-          </div>
-        </div>
-      </div>
-
-      {/* NAVIGATION ENTRE LES SECTIONS */}
-      <div className="border-t border-gray-200">
-        <nav className="flex border-b border-gray-200">
-          {tabs.map((tab) => (
-            <Link
-              key={tab.name}
-              to={`/profile/${id}/${tab.path}`}
-              className={`flex-1 py-4 text-center ${
-                location.pathname === `/profile/${id}/${tab.path}` 
-                  ? "border-b-2 border-blue-500 text-blue-500" 
-                  : "text-gray-500 hover:bg-gray-50"
-              }`}
-            >
-              {tab.name}
-            </Link>
-          ))}
-        </nav>
-
-        {/* Contenu dynamique des onglets */}
-        <div className="divide-y divide-gray-200 p-4">
-          <Outlet />
-        </div>
-
-        {/* Afficher mes tweet dans le composer */}
-        {tweets.length > 0 ? (
-          tweets.map((tweet) => (
-            <TweetCard
-              key={tweet._id}
-              tweet={tweet}
-              onLike={() => {}}
-              onRetweet={() => {}}
-              onReply={() => {}}
-              onBookmark={() => {}}
-            />
-          ))) :
-          (<>
-            <div className="text-center text-gray-500">Vous n'avez aucun tweet pour le moment.</div>
-          </>
-        )}
-      </div>
-    </>
+        ))
+      ) : (
+        <div className="no-tweets">Vous n'avez aucun tweet pour le moment.</div>
+      )}
+    </div>
   );
 }
